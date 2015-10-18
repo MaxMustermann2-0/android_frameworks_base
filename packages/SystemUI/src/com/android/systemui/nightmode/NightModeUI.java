17e241e9430aef647117a0815bdb7f8ca2be89ba
mm20 <maxmustermannbsp@gmail.com>
2015-10-18 17:56:56 +0200

    Let SystemUI handle night mode (1/2)
    Change-Id: I79f0b8e3a8d29db5638780d66377e938903ba515

--- packages/SystemUI/src/com/android/systemui/nightmode/NightModeUI.java
+++ packages/SystemUI/src/com/android/systemui/nightmode/NightModeUI.java
@@ -10,20 +10,30 @@ import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ThemeChangeRequest;
+import android.content.res.ThemeManager;
+import android.media.AudioManager;
 import android.os.Handler;
 import android.provider.Settings;
+import android.util.Log;
 
 /**
  * Created by mm20 on 18.10.15.
  */
 public class NightModeUI extends SystemUI {
     protected Handler mHandler = new Handler();
+    AudioManager mAudioManager;
+    ThemeManager mThemeManager;
     private NotificationManager mNotificationManager;
     private Intent mSettingsIntent;
+    private ContentResolver mContentResolver;
 
     @Override
     public void start() {
         mSettingsIntent = new Intent("android.settings.NIGHTMODE_SETTINGS");
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mThemeManager = (ThemeManager) mContext.getSystemService(Context.THEME_SERVICE);
+        mContentResolver = mContext.getContentResolver();
         mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
         SettingsObserver observer = new SettingsObserver(mHandler);
         observer.observe();
@@ -47,6 +57,170 @@ public class NightModeUI extends SystemUI {
         mNotificationManager.notify("nightmode_enabled", 101, builder.build());
     }
 
+    private void enableNightMode() {
+        boolean lowBrightness = Settings.System.getInt(mContentResolver, "nightmode_low_brightness",
+                0) == 1;
+        boolean disableBatteryLight = Settings.System.getInt(mContentResolver,
+                "nightmode_disable_battery_light", 0) == 1;
+        boolean disableNotificationLight = Settings.System.getInt(mContentResolver,
+                "nightmode_disable_notification_light", 0) == 1;
+        boolean liveDisplayNightMode = Settings.System.getInt(mContentResolver,
+                "nightmode_live_display_night", 0) == 1;
+        boolean useNightTheme = Settings.System.getInt(mContentResolver,
+                "nightmode_use_night_theme", 0) == 1;
+        int interruptions = Settings.System.getInt(mContentResolver, "nightmode_interruptions", -1);
+        boolean muteMedia = Settings.System.getInt(mContentResolver,
+                "nightmode_mute_media_sound", 0) == 1;
+
+        if (lowBrightness) {
+            setNightModePreference(Settings.System.SCREEN_BRIGHTNESS, 1, 100);
+            setNightModePreference(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        }
+        if (disableBatteryLight) {
+            setNightModePreference("battery_light_enabled", 0, 1);
+        }
+        if (disableNotificationLight) {
+            setNightModePreference("notification_light_pulse", 0, 1);
+        }
+        if (liveDisplayNightMode) {
+            int mode = Integer.parseInt(mContext.getResources().getStringArray(
+                    com.android.internal.R.array.live_display_values)[3]);
+            setNightModePreference(Settings.System.DISPLAY_TEMPERATURE_MODE, mode, 0);
+        }
+        if (useNightTheme) {
+            applyNightTheme();
+        }
+        if (interruptions != -1) {
+            int currentValue = Settings.Global.getInt(mContentResolver, "zen_mode", 0);
+            Settings.System.putInt(mContentResolver, "daymode_zen_mode", currentValue);
+            Settings.Global.putInt(mContentResolver, "zen_mode", interruptions);
+        }
+        if (muteMedia) {
+            int currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            Settings.System.putInt(mContentResolver, "daymode_media_volume", currentVolume);
+            mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+        }
+    }
+
+    private void disableNightMode() {
+        boolean lowBrightness = Settings.System.getInt(mContentResolver,
+                "nightmode_low_brightness", 0) == 1;
+        boolean disableBatteryLight = Settings.System.getInt(mContentResolver,
+                "nightmode_disable_battery_light", 0) == 1;
+        boolean disableNotificationLight = Settings.System.getInt(mContentResolver,
+                "nightmode_disable_notification_light", 0) == 1;
+        boolean liveDisplayNightMode = Settings.System.getInt(mContentResolver,
+                "nightmode_live_display_night", 0) == 1;
+        boolean useNightTheme = Settings.System.getInt(mContentResolver,
+                "nightmode_use_night_theme", 0) == 1;
+        int interruptions = Settings.System.getInt(mContentResolver, "nightmode_interruptions", -1);
+        boolean muteMedia = Settings.System.getInt(mContentResolver,
+                "nightmode_mute_media_sound", 0) == 1;
+        if (lowBrightness) {
+            setDayModePreference(Settings.System.SCREEN_BRIGHTNESS, 100);
+            setDayModePreference(Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+        }
+        if (disableBatteryLight) {
+            setDayModePreference("battery_light_enabled", 1);
+        }
+        if (disableNotificationLight) {
+            setDayModePreference("notification_light_pulse", 1);
+        }
+        if (liveDisplayNightMode) {
+            setDayModePreference(Settings.System.DISPLAY_TEMPERATURE_MODE, 0);
+        }
+        if (useNightTheme) {
+            applyDayTheme();
+        }
+        if (interruptions != -1) {
+            int newValue = Settings.System.getInt(mContentResolver, "daymode_zen_mode", 0);
+            Settings.Global.putInt(mContentResolver, "zen_mode", newValue);
+        }
+        if (muteMedia) {
+            int newVolume = Settings.System.getInt(mContentResolver, "daymode_media_volume", 0);
+            mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, newVolume, 0);
+        }
+    }
+
+    private void applyNightTheme() {
+
+        String currentStyle = Settings.System.getString(mContentResolver, "theme_current_overlay");
+        Settings.System.putString(mContentResolver, "daymode_theme_style", currentStyle);
+        String currentStatus = Settings.System.getString(mContentResolver, "theme_current_status");
+        Settings.System.putString(mContentResolver, "daymode_theme_status", currentStatus);
+        String currentNav = Settings.System.getString(mContentResolver, "theme_current_nav");
+        Settings.System.putString(mContentResolver, "daymode_theme_nav", currentNav);
+        String currentIcons = Settings.System.getString(mContentResolver, "theme_current_icons");
+        Settings.System.putString(mContentResolver, "daymode_theme_icons", currentIcons);
+        String currentWall = Settings.System.getString(mContentResolver, "theme_current_wallpaper");
+        Settings.System.putString(mContentResolver, "daymode_theme_wallpaper", currentWall);
+        String currentLock = Settings.System.getString(mContentResolver, "theme_current_lockscreen");
+        Settings.System.putString(mContentResolver, "daymode_theme_lockscreen", currentLock);
+
+        String nightStyle = Settings.System.getString(mContentResolver, "nightmode_theme_style");
+        String nightStatus = Settings.System.getString(mContentResolver, "nightmode_theme_status");
+        String nightNav = Settings.System.getString(mContentResolver, "nightmode_theme_nav");
+        String nightIcons = Settings.System.getString(mContentResolver, "nightmode_theme_icons");
+        String nightWall = Settings.System.getString(mContentResolver, "nightmode_theme_wallpaper");
+        String nightLock = Settings.System.getString(mContentResolver, "nightmode_theme_lockscreen");
+
+        ThemeChangeRequest.Builder builder = new ThemeChangeRequest.Builder();
+        if (nightStyle != null) builder.setOverlay(nightStyle);
+        if (nightStatus != null) builder.setOverlay(nightStatus);
+        if (nightNav != null) builder.setOverlay(nightNav);
+        if (nightIcons != null) builder.setOverlay(nightIcons);
+        if (nightWall != null) builder.setOverlay(nightWall);
+        if (nightLock != null) builder.setOverlay(nightLock);
+        mThemeManager.requestThemeChange(builder.build(), true);
+    }
+
+    private void applyDayTheme() {
+        String nightStyle = Settings.System.getString(mContentResolver, "nightmode_theme_style");
+        String nightStatus = Settings.System.getString(mContentResolver, "nightmode_theme_status");
+        String nightNav = Settings.System.getString(mContentResolver, "nightmode_theme_nav");
+        String nightIcons = Settings.System.getString(mContentResolver, "nightmode_theme_icons");
+        String nightWall = Settings.System.getString(mContentResolver, "nightmode_theme_wallpaper");
+        String nightLock = Settings.System.getString(mContentResolver, "nightmode_theme_lockscreen");
+
+        String newStyle = Settings.System.getString(mContentResolver, "daymode_theme_style");
+        String newStatus = Settings.System.getString(mContentResolver, "daymode_theme_status");
+        String newNav = Settings.System.getString(mContentResolver, "daymode_theme_nav");
+        String newIcons = Settings.System.getString(mContentResolver, "daymode_theme_icons");
+        String newWall = Settings.System.getString(mContentResolver, "daymode_theme_wallpaper");
+        String newLock = Settings.System.getString(mContentResolver, "daymode_theme_lockscreen");
+
+        newStyle = newStyle == null ? "system" : newStyle;
+        newStatus = newStatus == null ? "system" : newStatus;
+        newNav = newNav == null ? "system" : newNav;
+        newIcons = newIcons == null ? "system" : newIcons;
+        newWall = newWall == null ? "system" : newWall;
+        newLock = newLock == null ? "system" : newLock;
+
+        ThemeChangeRequest.Builder builder = new ThemeChangeRequest.Builder();
+        if (nightStyle != null) builder.setOverlay(newStyle);
+        if (nightStatus != null) builder.setOverlay(newStatus);
+        if (nightNav != null) builder.setOverlay(newNav);
+        if (nightIcons != null) builder.setOverlay(newIcons);
+        if (nightWall != null) builder.setOverlay(newWall);
+        if (nightLock != null) builder.setOverlay(newLock);
+        mThemeManager.requestThemeChange(builder.build(), false);
+    }
+
+
+    private void setNightModePreference(String key, int value, int defValue) {
+        int currentValue = Settings.System.getInt(mContentResolver, key, defValue);
+        Settings.System.putInt(mContentResolver, "daymode_" + key, currentValue);
+        Settings.System.putInt(mContentResolver, key, value);
+    }
+
+    private void setDayModePreference(String key, int defValue) {
+        int newValue = Settings.System.getInt(mContentResolver, "daymode_" + key, defValue);
+        Settings.System.putInt(mContentResolver, key, newValue);
+    }
+
     class SettingsObserver extends UserContentObserver {
         SettingsObserver(Handler handler) {
             super(handler);
@@ -62,9 +236,14 @@ public class NightModeUI extends SystemUI {
 
         @Override
         protected void update() {
-            boolean nightMode = Settings.System.getInt(mContext.getContentResolver(), "nightmode_enable_nightmode", 0) == 1;
-            if (nightMode) showNightModeNotification();
-            else mNotificationManager.cancel("nightmode_enabled", 101);
+            boolean nightMode = Settings.System.getInt(mContentResolver, "nightmode_enable_nightmode", 0) == 1;
+            if (nightMode) {
+                showNightModeNotification();
+                enableNightMode();
+            } else {
+                mNotificationManager.cancel("nightmode_enabled", 101);
+                disableNightMode();
+            }
         }
     }
 }
