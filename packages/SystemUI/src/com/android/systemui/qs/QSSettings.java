9fff7f5be4d8d0d0d82d1a8fc63525a4b9ab6359
MaxMustermann2.0 <maxmustermannbsp@gmail.com>
2016-02-24 16:44:35 +0100

    SystemUI: New design
    Change-Id: I1c42bbc33395f6de909a1c0b911e18d5d32af8b7

--- packages/SystemUI/src/com/android/systemui/qs/QSSettings.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSSettings.java
@@ -19,10 +19,17 @@ import android.annotation.Nullable;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.ThemeChangeRequest;
+import android.content.res.ThemeManager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateXAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
@@ -30,11 +37,20 @@ import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QSTileHost;
 import com.android.systemui.statusbar.phone.SystemUIDialog;
 
-public class QSSettings extends ScrollView {
+public class QSSettings extends ScrollView implements View.OnClickListener {
 
     private QSTileHost mHost;
 
+    private View mBluegrey;
+    private View mRed;
+    private View mBlue;
+    private View mTeal;
+    private View mOrange;
+    private View mCyan;
+    private View mCurrent;
+
     private boolean mAdapterEditingState;
+    private boolean mColorSchemeOpen;
 
     public QSSettings(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -51,6 +67,27 @@ public class QSSettings extends ScrollView {
                 initiateTileReset();
             }
         });
+        findViewById(R.id.qs_color_scheme).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorScheme();
+            }
+        });
+        mBluegrey = findViewById(R.id.color_scheme_bluegrey);
+        mRed = findViewById(R.id.color_scheme_red);
+        mBlue = findViewById(R.id.color_scheme_blue);
+        mTeal = findViewById(R.id.color_scheme_teal);
+        mOrange = findViewById(R.id.color_scheme_orange);
+        mCyan = findViewById(R.id.color_scheme_cyan);
+        mCurrent = findViewById(R.id.color_scheme_current);
+
+        mBluegrey.setOnClickListener(this);
+        mRed.setOnClickListener(this);
+        mBlue.setOnClickListener(this);
+        mTeal.setOnClickListener(this);
+        mOrange.setOnClickListener(this);
+        mCyan.setOnClickListener(this);
+        mCurrent.setOnClickListener(this);
     }
 
     private void initiateTileReset() {
@@ -78,5 +115,99 @@ public class QSSettings extends ScrollView {
 
     public void setAdapterEditingState(boolean editing) {
         this.mAdapterEditingState = editing;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mCurrent) {
+            if(mColorSchemeOpen){
+                closeColorScheme();
+            }else{
+                openColorScheme();
+            }
+            return;
+        }
+        ThemeChangeRequest.Builder builder = new ThemeChangeRequest.Builder();
+        ThemeManager themeManager = (ThemeManager) getContext()
+                .getSystemService(Context.THEME_SERVICE);
+        String themePkg = "system";
+        if(view == mRed) themePkg = "de.mm20.sysuired";
+        if(view == mBlue) themePkg = "de.mm20.sysuiblue";
+        if(view == mTeal) themePkg = "de.mm20.sysuiteal";
+        if(view == mCyan) themePkg = "de.mm20.sysuicyan";
+        if(view == mOrange) themePkg = "de.mm20.sysuiorange";
+        builder.setStatusBar(themePkg);
+        themeManager.requestThemeChange(builder.build(), false);
+    }
+
+    private void openColorScheme(){
+        mColorSchemeOpen = true;
+        mBluegrey.setVisibility(VISIBLE);
+        mBlue.setVisibility(VISIBLE);
+        mRed.setVisibility(VISIBLE);
+        mTeal.setVisibility(VISIBLE);
+        mOrange.setVisibility(VISIBLE);
+        mCyan.setVisibility(VISIBLE);
+        AlphaAnimation anim1 = new AlphaAnimation(0, 1);
+        anim1.setDuration(200);
+        AlphaAnimation anim2 = new AlphaAnimation(0, 1);
+        anim2.setDuration(200);
+        AlphaAnimation anim3 = new AlphaAnimation(0, 1);
+        anim3.setDuration(200);
+        AlphaAnimation anim4 = new AlphaAnimation(0, 1);
+        anim4.setDuration(200);
+        AlphaAnimation anim5 = new AlphaAnimation(0, 1);
+        anim5.setDuration(200);
+        AlphaAnimation anim6 = new AlphaAnimation(0, 1);
+        anim6.setDuration(200);
+        mBlue.startAnimation(anim1);
+        mBluegrey.startAnimation(anim2);
+        mTeal.startAnimation(anim3);
+        mRed.startAnimation(anim4);
+        mCyan.startAnimation(anim5);
+        mOrange.startAnimation(anim6);
+    }
+
+    private void closeColorScheme(){
+        mColorSchemeOpen = false;
+        AlphaAnimation anim1 = new AlphaAnimation(1, 0);
+        anim1.setDuration(200);
+        anim1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mBluegrey.setVisibility(INVISIBLE);
+                mBlue.setVisibility(INVISIBLE);
+                mRed.setVisibility(INVISIBLE);
+                mTeal.setVisibility(INVISIBLE);
+                mCyan.setVisibility(INVISIBLE);
+                mOrange.setVisibility(INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        AlphaAnimation anim2 = new AlphaAnimation(1, 0);
+        anim2.setDuration(200);
+        AlphaAnimation anim3 = new AlphaAnimation(1, 0);
+        anim3.setDuration(200);
+        AlphaAnimation anim4 = new AlphaAnimation(1, 0);
+        anim4.setDuration(200);
+        AlphaAnimation anim5 = new AlphaAnimation(1, 0);
+        anim5.setDuration(200);
+        AlphaAnimation anim6 = new AlphaAnimation(1, 0);
+        anim6.setDuration(200);
+        mBlue.startAnimation(anim1);
+        mBluegrey.startAnimation(anim2);
+        mTeal.startAnimation(anim3);
+        mRed.startAnimation(anim4);
+        mOrange.startAnimation(anim5);
+        mCyan.startAnimation(anim6);
     }
 }
